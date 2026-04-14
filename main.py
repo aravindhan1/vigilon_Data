7131105820823c8530ac96e6e0db97d29d792d71
aravindhan1 <aravindh5445@gmail.com>
2026-04-14 18:19:31 +0100

    Project push

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 # main.py
 
 from drivers.mpu6050 import MPU6050
-from drivers.ds18b20 import DS18B20
+
 import time
 
 mpu = MPU6050()
-temp_sensor = DS18B20()
 
 while True:
     ax, ay, az = mpu.read_accel()
     gx, gy, gz = mpu.read_gyro()
-    temp = temp_sensor.read_temp()
 
     print("------ SENSOR DATA ------")
-    print(f"Temperature: {temp:.2f} °C")
     print(f"Accel: X={ax}, Y={ay}, Z={az}")
     print(f"Gyro: X={gx}, Y={gy}, Z={gz}")
 
